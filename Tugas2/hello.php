ee99e182e94ab31501610bbeeee6b39758029539
Rosita19 <200411100002@student.trunojoyo.ac.id>
2022-03-11 11:16:32 +0700

    menambahkan file hello.php

---
+++ Tugas2/hello.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tugas 1 PAW</title>
+</head>
+<body>
+	<?php
+	$nama = "Rosita Dewi Lutfiyah"; //membuat variable nama yang memiliki value "Rosita Dewi Lutfiyah"
+	$nim = "200411100002"; //membuat variable nim yang memiliki value "200411100002"
+
+	echo "Nama : $nama <br>"; //menampilkan variable $nama
+	echo "Nim : $nim"; //menampilkan variable nim
+	?>
+	
+</body>
+</html>
