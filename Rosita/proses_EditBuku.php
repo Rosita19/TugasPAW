6ad800f0cf7c70929563c95457e1e0a5fa420cb8
Rosita19 <200411100002@student.trunojoyo.ac.id>
2022-03-31 12:24:47 +0700

    memasukkan file proses_EditBuku.php

---
+++ Rosita/proses_EditBuku.php
@@ -0,0 +1,22 @@
+<?php 
+
+	include "koneksi.php";
+
+	if (isset($_POST['submit'])) {
+		$kd_buku	= $_GET['kd_buku'];
+		$judul_buku	= $_POST['judul_buku'];
+		$pengarang	= $_POST['pengarang'];
+		$penerbit	= $_POST['penerbit'];
+		$tahun		= $_POST['tahun'];
+		$harga		= $_POST['harga'];
+
+		$query = "UPDATE tbl_002 SET judul_buku = '$judul_buku', pengarang = '$pengarang', penerbit = '$penerbit', tahun = '$tahun', harga = '$harga' WHERE kd_buku = '$kd_buku'";
+		$sql = mysqli_query($konek, $query);
+		if ($sql) {
+		    	header("location:dataBuku.php");
+		    }else{
+		    	echo "Terjadi kesalahan saat melakukan pengeditan";
+		    	echo "</br><a href = 'form_EditBuku.php'>Kembali ke form edit</a>";
+		    }    
+	}
+?>
