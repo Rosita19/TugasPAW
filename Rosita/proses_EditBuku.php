9d056baa530cf504f7151f0feba3470da5532372
Rosita19 <200411100002@student.trunojoyo.ac.id>
2022-03-31 17:15:39 +0700

    mengedit file proses_EditBuku.php

--- Rosita/proses_EditBuku.php
+++ Rosita/proses_EditBuku.php
@@ -13,7 +13,7 @@
 		$query = "UPDATE tbl_002 SET judul_buku = '$judul_buku', pengarang = '$pengarang', penerbit = '$penerbit', tahun = '$tahun', harga = '$harga' WHERE kd_buku = '$kd_buku'";
 		$sql = mysqli_query($konek, $query);
 		if ($sql) {
-		    	header("location:dataBuku.php");
+		    	echo "<script>alert('Berhasil Mengedit Data');window.location = 'dataBuku.php'</script>";
 		    }else{
 		    	echo "Terjadi kesalahan saat melakukan pengeditan";
 		    	echo "</br><a href = 'form_EditBuku.php'>Kembali ke form edit</a>";
