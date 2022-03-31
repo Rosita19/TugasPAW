19414cf56539f1bca3671a448ce940e762e8b9d4
Rosita19 <200411100002@student.trunojoyo.ac.id>
2022-03-31 12:18:26 +0700

    memasukkan file proses_InputBuku.php

---
+++ Rosita/proses_InputBuku.php
@@ -0,0 +1,21 @@
+<?php 
+	include "koneksi.php";
+
+	if (isset($_POST['submit'])) {
+		$kd_buku	= $_POST['kd_buku'];
+		$judul_buku	= $_POST['judul_buku'];
+		$pengarang	= $_POST['pengarang'];
+		$penerbit	= $_POST['penerbit'];
+		$tahun		= $_POST['tahun'];
+		$harga		= $_POST['harga'];
+
+		$query = "INSERT INTO tbl_002 (kd_buku, judul_buku, pengarang, penerbit, tahun, harga) VALUES ('".$kd_buku."', '".$judul_buku."', '".$pengarang."', '".$penerbit."', '".$tahun."', '".$harga."')";
+		$sql = mysqli_query($konek, $query);
+		if ($sql) {
+		 	header("location:dataBuku.php");
+		 }else{
+		 	echo "Terjadi kesalahan saat masuk database";
+		 	echo "</br><a href = 'index.php'>Kembali ke form Input Buku</a>";
+		 }
+	}
+?>
