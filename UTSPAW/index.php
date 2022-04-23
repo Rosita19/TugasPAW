e1d34a4a1bc8a6c519abd9b46ba42ba9f28300db
Rosita19 <iqbalbaihaqi319@gmail.com>
2022-04-23 14:07:27 +0700

    menambahkan folder

---
+++ UTSPAW/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>UTS PAW</title>
+</head>
+<body>
+	<center><h1><b>Form Input Data</b></h1></center>
+	<form action="prosesData.php" method="post">
+		<center><table border="0">
+			<tr><td>Id 002</td>
+				<td><input type="text" name="id_002" placeholder="Id 002" style="padding-right: 50px;"></td></tr>
+			<tr><td>Nama 002</td>
+				<td><input type="text" name="nama_002" placeholder="Nama 002" style="padding-right: 50px;"></td></tr>
+			<tr><td>Email 002</td>
+				<td><input type="text" name="email_002" placeholder="Email 002" style="padding-right: 50px;"></td></tr>
+			<tr>
+				<td><input type="submit" name="submit" value="Simpan">
+				<input type="reset" value="Clear">
+				<a href="dataRosita.php"><input type="button" value="Batal"></a></td>
+			</tr>
+		</table>
+	</form></center>
+
+</body>
+</html>
